7ff87c8993ee7f21f08b12ba0c7c93265ba08845
aimeouraga <oballou@aimsammi.org>
2024-10-16 22:39:58 +0000

    Adding testing

---
+++ Tests/test_predict_no_auth.py
@@ -0,0 +1,11 @@
+import pytest
+from test import client
+
+
+def test_predict_no_auth():
+    response = client.post(
+        "/predict/",
+        data={"text": "I am so happy!"}
+    )
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Not authenticated"}
---
+++ Tests/test_read_root.py
@@ -0,0 +1,12 @@
+import pytest
+
+from test import client 
+
+
+
+# Test the root endpoint
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to the Emotion detection API"}
+
